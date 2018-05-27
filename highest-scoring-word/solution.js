e277fdb9b712246d82849c3c00d1284d62e41d85
Benjamin Taylor <36553965+btaylor93@users.noreply.github.com>
2018-05-26 17:19:27 -0700

    fixed whitespace problem

--- highest-scoring-word/solution.js
+++ highest-scoring-word/solution.js
@@ -1,5 +1,5 @@
 function high(x){
-  if (!x.match(/\w+/g)) { return x }
+  if (!x.match(/\w+/g)) { return '' }
   return x.match(/\w+/g).map(word => {
     return [word, word.split('').map(char => char.charCodeAt() - 96).reduce((acc, cur) => acc + cur)]
   }).sort((a, b) => b[1] - a[1])[0][0]
